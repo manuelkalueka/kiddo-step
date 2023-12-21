952e7e370ca1d61fd89b7a57f11f727883fd8a44
Manuel Kalueka <pedrokalueca@gmail.com>
2023-12-21 14:37:31 +0100

    REFACTOR: Estilos padrões criados

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import defaultStyle from "./src/defaultStyle";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar
+          backgroundColor={defaultStyle.colors.blueDarkColor4}
+          barStyle="light-content"
+        />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: defaultStyle.colors.white,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
