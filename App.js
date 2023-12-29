5ac87233886d758f0d72d71baa2cc4bfe5b05632
Manuel Kalueka <pedrokalueca@gmail.com>
2023-12-29 17:27:32 +0100

    FEAT: tela principal de cadastro criada

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Platform } from "react-native";
+import { StyleSheet, Platform } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import defaultStyle from "./src/defaultStyle";
-
 import Login from "./src/components/pages/Login";
+import Signup from "./src/components/pages/Signup";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <StatusBar style="auto" />
-        <View>
-          <Login />
-        </View>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Signup"
+          >
+            <Stack.Screen name="Signup" component={Signup}
+            options={
+              {
+                headerShown:false,
+              }
+            }
+            />
+              <Stack.Screen name="Login" component={Login}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -22,6 +36,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: defaultStyle.colors.white,
-    paddingTop: Platform.OS == "android" ? 40 : 0,
   },
 });
---
+++ src/components/pages/Signup/styles.js
@@ -0,0 +1,79 @@
+import { StyleSheet } from "react-native";
+import defaultStyles from "../../../defaultStyle";
+
+const styles = StyleSheet.create({
+  loginContainer: {
+    flex: 1,
+    backgroundColor: defaultStyles.colors.white,
+    paddingVertical: "5%",
+    paddingHorizontal: "5%",
+  },
+
+  titleContainer: {
+    marginTop: "10%",
+  },
+
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    height: "auto",
+    borderRadius: defaultStyles.borderRadio.borderRadioInput,
+    borderWidth: 1,
+    marginVertical: 10,
+    paddingLeft: 10,
+  },
+
+  input: {
+    paddingVertical: 16,
+    fontSize: 16,
+    flex: 1,
+  },
+
+  inputFocused: {
+    borderColor: defaultStyles.colors.mainColorBue,
+  },
+
+  inputDefault: {
+    borderColor: defaultStyles.colors.grayAccent1,
+  },
+
+  inputIcon: {
+    marginRight: 10,
+  },
+  MainButton: {
+    width: "100%",
+    marginTop: 15,
+    padding: 16,
+    backgroundColor: defaultStyles.colors.mainColorBue,
+    borderRadius: defaultStyles.borderRadio.borderRadioButton.small,
+  },
+  textButton: {
+    textAlign: "center",
+    fontSize: defaultStyles.sizes.subtitle,
+    color: defaultStyles.colors.white,
+    fontWeight: "bold",
+  },
+  haveAccountContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 36,
+  },
+  haveAccountText: {
+    fontWeight: "400",
+    fontSize: defaultStyles.sizes.subtitle,
+  },
+  buttonAction: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    alignItems: "center",
+  },
+  haveAccounAction: {
+    color: defaultStyles.colors.mainColorBue,
+    fontWeight: "bold",
+    fontSize: defaultStyles.sizes.subtitle,
+  },
+});
+
+export default styles;
