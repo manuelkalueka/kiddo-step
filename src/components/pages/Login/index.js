bbb2014030edeebe18dbdb8aa268537e855a594c
Tiago Bondo <118920546+tiagobondo@users.noreply.github.com>
2024-01-07 22:15:40 +0100

    estrutura do componente login e sua estilização

---
+++ src/components/pages/Login/index.js
@@ -0,0 +1,17 @@
+import {
+      View,
+      Text,
+      TextInput,
+      TouchableOpacity
+} from 'react-native';
+
+import {AntDesign} from '@expo/vector-icons';
+
+import {styles} from './style'
+
+export default function Login(){
+  return(
+    <View>
+
+    </View>
+  )}
