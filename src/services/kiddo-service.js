3ca03584916e2cc03b49d4fba22f1b787665a80b
Tiago Bondo <118920546+tiagobondo@users.noreply.github.com>
2024-04-17 12:22:34 +0100

    Reajuste.

--- src/services/kiddo-service.js
+++ src/services/kiddo-service.js
@@ -1,9 +1,66 @@
-// import ApiMananger from "./api";
+import ApiMananger from "./api";
+import { useAuth } from "../contexts/auth";
 
-function getKiddo() {
-  const data = "65fc647b669b25ce331d8700";
-  const kiddo = data;
-  return kiddo;
+async function getKiddoInfo(id) {
+  const { user } = useAuth();
+  try {
+    const { data } = await ApiMananger.get(`/kiddo/${id}/${user._id}`);
+    return data;
+  } catch (error) {
+    console.log("Erro ao Buscar criança ", error);
+  }
 }
 
-export { getKiddo };
+async function setKiddoInfo(kiddoData, user) {
+  try {
+    const {
+      fullName,
+      surname,
+      birthDate,
+      gendre,
+      avatar,
+      bloodType,
+      alergics,
+    } = kiddoData;
+    await ApiMananger.post(`/kiddo`, {
+      fullName,
+      surname,
+      birthDate,
+      gendre,
+      avatar,
+      bloodType,
+      alergics,
+      parent: user._id,
+    });
+  } catch (error) {
+    console.log("Erro ao salvar criança ", error);
+  }
+}
+
+async function editKiddoInfo(id, user, kiddoData) {
+  try {
+    const {
+      fullName,
+      surname,
+      birthDate,
+      gendre,
+      avatar,
+      bloodType,
+      alergics,
+    } = kiddoData;
+    await ApiMananger.put(`/kiddo/${id}`, {
+      fullName,
+      surname,
+      birthDate,
+      gendre,
+      avatar,
+      bloodType,
+      alergics,
+      parent: user._id,
+    });
+  } catch (error) {
+    console.log("Erro ao Alterar dados da Criança ", error);
+  }
+}
+
+export { getKiddoInfo, setKiddoInfo, editKiddoInfo };
